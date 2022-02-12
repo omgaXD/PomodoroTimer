aea68254331ceb72706c3915322d5f7df247161b
omgaXD <sladekop@gmail.com>
2022-02-12 23:43:15 +0500

    Добавлена английская локализация (90% проекта).

--- PomodoroTechnique/PomodoroSetup.Designer.cs
+++ PomodoroTechnique/PomodoroSetup.Designer.cs
@@ -60,6 +60,7 @@
             // 
             // groupBox2
             // 
+            resources.ApplyResources(groupBox2, "groupBox2");
             groupBox2.Controls.Add(this.labelMelody);
             groupBox2.Controls.Add(this.melodyPathTextBox);
             groupBox2.Controls.Add(this.importMusicButton);
@@ -74,72 +75,51 @@
             groupBox2.Controls.Add(this.nameTextBox);
             groupBox2.Controls.Add(this.upButton);
             groupBox2.Controls.Add(this.downButton);
-            groupBox2.Location = new System.Drawing.Point(14, 12);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new System.Drawing.Size(258, 314);
-            groupBox2.TabIndex = 11;
             groupBox2.TabStop = false;
-            groupBox2.Text = "Настройки таймера";
             // 
             // labelMelody
             // 
-            this.labelMelody.AutoSize = true;
-            this.labelMelody.Location = new System.Drawing.Point(6, 246);
+            resources.ApplyResources(this.labelMelody, "labelMelody");
             this.labelMelody.Name = "labelMelody";
-            this.labelMelody.Size = new System.Drawing.Size(55, 13);
-            this.labelMelody.TabIndex = 14;
-            this.labelMelody.Text = "Мелодия:";
             // 
             // melodyPathTextBox
             // 
-            this.melodyPathTextBox.Location = new System.Drawing.Point(62, 243);
+            resources.ApplyResources(this.melodyPathTextBox, "melodyPathTextBox");
             this.melodyPathTextBox.Name = "melodyPathTextBox";
             this.melodyPathTextBox.ReadOnly = true;
-            this.melodyPathTextBox.Size = new System.Drawing.Size(125, 20);
-            this.melodyPathTextBox.TabIndex = 13;
             // 
             // importMusicButton
             // 
-            this.importMusicButton.Location = new System.Drawing.Point(186, 242);
+            resources.ApplyResources(this.importMusicButton, "importMusicButton");
             this.importMusicButton.Name = "importMusicButton";
-            this.importMusicButton.Size = new System.Drawing.Size(66, 22);
-            this.importMusicButton.TabIndex = 12;
-            this.importMusicButton.Text = "Выбрать";
             this.importMusicButton.UseVisualStyleBackColor = true;
             this.importMusicButton.Click += new System.EventHandler(this.importMusicButton_Click);
             // 
             // exportButton
             // 
-            this.exportButton.Location = new System.Drawing.Point(134, 283);
+            resources.ApplyResources(this.exportButton, "exportButton");
             this.exportButton.Name = "exportButton";
-            this.exportButton.Size = new System.Drawing.Size(118, 23);
-            this.exportButton.TabIndex = 11;
-            this.exportButton.Text = "Экспорт настроек";
             this.exportButton.UseVisualStyleBackColor = true;
             this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
             // 
             // importButton
             // 
-            this.importButton.Location = new System.Drawing.Point(6, 283);
+            resources.ApplyResources(this.importButton, "importButton");
             this.importButton.Name = "importButton";
-            this.importButton.Size = new System.Drawing.Size(122, 23);
-            this.importButton.TabIndex = 10;
-            this.importButton.Text = "Импорт настроек";
             this.importButton.UseVisualStyleBackColor = true;
             this.importButton.Click += new System.EventHandler(this.importButton_Click);
             // 
             // timerPeriodsBox
             // 
+            resources.ApplyResources(this.timerPeriodsBox, "timerPeriodsBox");
             this.timerPeriodsBox.FormattingEnabled = true;
-            this.timerPeriodsBox.Location = new System.Drawing.Point(15, 19);
             this.timerPeriodsBox.Name = "timerPeriodsBox";
-            this.timerPeriodsBox.Size = new System.Drawing.Size(225, 108);
-            this.timerPeriodsBox.TabIndex = 1;
             this.timerPeriodsBox.SelectedIndexChanged += new System.EventHandler(this.timerPeriodsBox_SelectedIndexChanged);
             // 
             // timeNumeric
             // 
-            this.timeNumeric.Location = new System.Drawing.Point(89, 193);
+            resources.ApplyResources(this.timeNumeric, "timeNumeric");
             this.timeNumeric.Maximum = new decimal(new int[] {
             60,
             0,
@@ -151,8 +131,6 @@
             0,
             0});
             this.timeNumeric.Name = "timeNumeric";
-            this.timeNumeric.Size = new System.Drawing.Size(41, 20);
-            this.timeNumeric.TabIndex = 9;
             this.timeNumeric.Value = new decimal(new int[] {
             1,
             0,
@@ -162,119 +140,81 @@
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(15, 133);
+            resources.ApplyResources(this.addButton, "addButton");
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(68, 23);
-            this.addButton.TabIndex = 3;
-            this.addButton.Text = "Добавить";
             this.addButton.UseVisualStyleBackColor = true;
             this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 200);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Время (мин.):";
             // 
             // removeButton
             // 
-            this.removeButton.Location = new System.Drawing.Point(89, 133);
+            resources.ApplyResources(this.removeButton, "removeButton");
             this.removeButton.Name = "removeButton";
-            this.removeButton.Size = new System.Drawing.Size(68, 23);
-            this.removeButton.TabIndex = 2;
-            this.removeButton.Text = "Удалить";
             this.removeButton.UseVisualStyleBackColor = true;
             this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 174);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Название:";
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(89, 167);
+            resources.ApplyResources(this.nameTextBox, "nameTextBox");
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(110, 20);
-            this.nameTextBox.TabIndex = 6;
             this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
             // 
             // upButton
             // 
-            this.upButton.Location = new System.Drawing.Point(163, 133);
+            resources.ApplyResources(this.upButton, "upButton");
             this.upButton.Name = "upButton";
-            this.upButton.Size = new System.Drawing.Size(35, 23);
-            this.upButton.TabIndex = 4;
-            this.upButton.Text = "↑";
             this.upButton.UseVisualStyleBackColor = true;
             this.upButton.Click += new System.EventHandler(this.upButton_Click);
             // 
             // downButton
             // 
-            this.downButton.Location = new System.Drawing.Point(204, 133);
+            resources.ApplyResources(this.downButton, "downButton");
             this.downButton.Name = "downButton";
-            this.downButton.Size = new System.Drawing.Size(35, 23);
-            this.downButton.TabIndex = 5;
-            this.downButton.Text = "↓";
             this.downButton.UseVisualStyleBackColor = true;
             this.downButton.Click += new System.EventHandler(this.downButton_Click);
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(29, 405);
+            resources.ApplyResources(this.startButton, "startButton");
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(225, 23);
-            this.startButton.TabIndex = 0;
-            this.startButton.Text = "Начать таймер";
             this.startButton.UseVisualStyleBackColor = true;
             this.startButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // RBrunOnce
             // 
-            this.RBrunOnce.AutoSize = true;
+            resources.ApplyResources(this.RBrunOnce, "RBrunOnce");
             this.RBrunOnce.Checked = true;
-            this.RBrunOnce.Location = new System.Drawing.Point(29, 330);
             this.RBrunOnce.Name = "RBrunOnce";
-            this.RBrunOnce.Size = new System.Drawing.Size(134, 17);
-            this.RBrunOnce.TabIndex = 12;
             this.RBrunOnce.TabStop = true;
-            this.RBrunOnce.Text = "Не повторять таймер";
             this.RBrunOnce.UseVisualStyleBackColor = true;
             this.RBrunOnce.CheckedChanged += new System.EventHandler(this.RBrunOnce_CheckedChanged);
             // 
             // RBrepeatN
             // 
-            this.RBrepeatN.AutoSize = true;
-            this.RBrepeatN.Location = new System.Drawing.Point(29, 354);
+            resources.ApplyResources(this.RBrepeatN, "RBrepeatN");
             this.RBrepeatN.Name = "RBrepeatN";
-            this.RBrepeatN.Size = new System.Drawing.Size(157, 17);
-            this.RBrepeatN.TabIndex = 13;
-            this.RBrepeatN.Text = "Повторить                    раз";
             this.RBrepeatN.UseVisualStyleBackColor = true;
             this.RBrepeatN.CheckedChanged += new System.EventHandler(this.RBrepeatN_CheckedChanged);
             // 
             // RBrepeatInfinitely
             // 
-            this.RBrepeatInfinitely.AutoSize = true;
-            this.RBrepeatInfinitely.Location = new System.Drawing.Point(29, 377);
+            resources.ApplyResources(this.RBrepeatInfinitely, "RBrepeatInfinitely");
             this.RBrepeatInfinitely.Name = "RBrepeatInfinitely";
-            this.RBrepeatInfinitely.Size = new System.Drawing.Size(181, 17);
-            this.RBrepeatInfinitely.TabIndex = 14;
-            this.RBrepeatInfinitely.Text = "Повторять таймер бесконечно";
             this.RBrepeatInfinitely.UseVisualStyleBackColor = true;
             this.RBrepeatInfinitely.CheckedChanged += new System.EventHandler(this.RBrepeatInfinitely_CheckedChanged);
             // 
             // runTimerCount
             // 
-            this.runTimerCount.Enabled = false;
-            this.runTimerCount.Location = new System.Drawing.Point(110, 354);
+            resources.ApplyResources(this.runTimerCount, "runTimerCount");
             this.runTimerCount.Maximum = new decimal(new int[] {
             999,
             0,
@@ -286,8 +226,6 @@
             0,
             0});
             this.runTimerCount.Name = "runTimerCount";
-            this.runTimerCount.Size = new System.Drawing.Size(41, 20);
-            this.runTimerCount.TabIndex = 10;
             this.runTimerCount.Value = new decimal(new int[] {
             2,
             0,
@@ -298,42 +236,34 @@
             // 
             this.saveFileDialog1.DefaultExt = "pts";
             this.saveFileDialog1.FileName = "PomodoroTimerSettings";
-            this.saveFileDialog1.Filter = "Pomodoro Timer Settings files|*.pts";
-            this.saveFileDialog1.Title = "Сохранить Настройки Таймера";
+            resources.ApplyResources(this.saveFileDialog1, "saveFileDialog1");
             this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.DefaultExt = "pts";
             this.openFileDialog1.FileName = "PomodoroTimerSettings";
-            this.openFileDialog1.Filter = "Pomodoro Timer Settings files|*.pts";
-            this.openFileDialog1.Title = "Открыть Настройки Таймера";
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // openMelodyDialog
             // 
             this.openMelodyDialog.DefaultExt = "mp3";
-            this.openMelodyDialog.Filter = "MP3 Файлы|*.mp3|WAVE Файлы|*.wav";
-            this.openMelodyDialog.Title = "Выбрать мелодию";
+            resources.ApplyResources(this.openMelodyDialog, "openMelodyDialog");
             this.openMelodyDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openMelodyDialog_FileOk);
             // 
             // PomodoroSetup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 438);
             this.Controls.Add(this.runTimerCount);
             this.Controls.Add(this.RBrepeatInfinitely);
             this.Controls.Add(this.RBrepeatN);
             this.Controls.Add(this.RBrunOnce);
             this.Controls.Add(groupBox2);
             this.Controls.Add(this.startButton);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(300, 477);
-            this.MinimumSize = new System.Drawing.Size(300, 477);
             this.Name = "PomodoroSetup";
-            this.Text = "Pomodoro Timer";
             groupBox2.ResumeLayout(false);
             groupBox2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.timeNumeric)).EndInit();
--- PomodoroTechnique/PomodoroTimer.Designer.cs
+++ PomodoroTechnique/PomodoroTimer.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PomodoroTimer));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timerLabel = new System.Windows.Forms.Label();
             this.exitButton = new System.Windows.Forms.Button();
@@ -40,49 +41,28 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(225, 225);
-            this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // timerLabel
             // 
-            this.timerLabel.AutoSize = true;
-            this.timerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.timerLabel.Location = new System.Drawing.Point(31, 240);
-            this.timerLabel.MaximumSize = new System.Drawing.Size(182, 46);
-            this.timerLabel.MinimumSize = new System.Drawing.Size(182, 46);
+            resources.ApplyResources(this.timerLabel, "timerLabel");
             this.timerLabel.Name = "timerLabel";
-            this.timerLabel.Size = new System.Drawing.Size(182, 46);
-            this.timerLabel.TabIndex = 3;
-            this.timerLabel.Text = "00:00:00";
-            this.timerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(39, 350);
+            resources.ApplyResources(this.exitButton, "exitButton");
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(174, 23);
-            this.exitButton.TabIndex = 4;
-            this.exitButton.Text = "Завершить таймер";
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // activityLabel
             // 
-            this.activityLabel.AutoSize = true;
+            resources.ApplyResources(this.activityLabel, "activityLabel");
             this.activityLabel.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.activityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.activityLabel.ForeColor = System.Drawing.Color.LimeGreen;
-            this.activityLabel.Location = new System.Drawing.Point(-8, 286);
-            this.activityLabel.MinimumSize = new System.Drawing.Size(265, 46);
             this.activityLabel.Name = "activityLabel";
-            this.activityLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.activityLabel.Size = new System.Drawing.Size(265, 46);
-            this.activityLabel.TabIndex = 5;
-            this.activityLabel.Text = "Отдых";
-            this.activityLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // timer1
             // 
@@ -91,33 +71,23 @@
             // 
             // button1
             // 
-            this.button1.Enabled = false;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(39, 288);
+            resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(174, 42);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Продолжить";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // PomodoroTimer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(249, 385);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.activityLabel);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.timerLabel);
             this.Controls.Add(this.pictureBox1);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(265, 424);
-            this.MinimumSize = new System.Drawing.Size(265, 424);
             this.Name = "PomodoroTimer";
             this.ShowIcon = false;
-            this.Text = "PomodoroTimer";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
